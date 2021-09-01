da381b89cef19c325b2d1583f5eaba51b45761a5
Nor-Mand <normandlaredo@gmail.com>
2021-09-01 14:23:04 -0400

    Add folder CRM to custom button in the form view

---
+++ odoons_crm/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoons CRM",
+    'summary':"""Module to custom fields, views, models, inherit""",
+    'author': "Nor-Mand",
+    'depends':['crm'],
+    'data':[
+        'views/odoons_crm_view.xml',
+    ],
+}
