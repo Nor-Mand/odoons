6dded5de57525e38f5376fbac3c7bc484131990e
Nor-Mand <normandlaredo@gmail.com>
2021-09-01 17:06:57 -0400

    add the folder odoons_sale_crm

---
+++ odoons_sale_crm/__manifest__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoons Sale CRM",
+    'summary': """This module correct the sum of orders in the button stat in CRM form""",
+    'author':"Nor-Mand",
+    'depends': ['sale_management', 'crm'],
+}
