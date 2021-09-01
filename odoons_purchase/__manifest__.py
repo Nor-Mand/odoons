a467a83af1d9c16749ba2fba4585a75da9cc7a93
Nor-Mand <normandlaredo@gmail.com>
2021-08-31 23:44:27 -0400

    Start Project Odoo

---
+++ odoons_purchase/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoons Purchase",
+    'summary': """Module to change fields in reports and vieww list""",
+    'author': "Nor_Mand",
+    'depends': ['purchase'],
+    'data': [
+        'views/odoons_purchase_view.xml',
+    ],
+}
