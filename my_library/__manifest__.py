3bab34cb56388ba40f292dccdfb24911c11fd420
Nor-Mand <normandlaredo@gmail.com>
2021-09-05 22:41:52 -0400

    add folder my_library tutorial odoo14

---
+++ my_library/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "My Library",
+    'summary': """Manage books easily""",
+    'description': """
+        Manage Library
+        ==============
+        Description related to library
+    """,
+    'author': "Nor-Mand",
+    'website': "http://www.normandweb.com",
+    'category': "Uncategorized",
+    'version': '13.0.1',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo':[
+        'demo.xml'
+    ],
+}
