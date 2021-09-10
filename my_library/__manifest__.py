0089073ca4b38be96a46c70ef9f83b0f28da3517
Nor-Mand <normandlaredo@gmail.com>
2021-09-10 15:58:14 -0400

    add security access to model

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -9,9 +9,9 @@
     'version': '13.0.1',
     'depends': ['base'],
     'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/library_book.xml',
     ],
-    'demo':[
-        'demo.xml',
-    ],
+    'application': True,
 }
